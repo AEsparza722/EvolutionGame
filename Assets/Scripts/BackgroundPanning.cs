6ca0c9dc7636795aa249bfe345891850ef58a4f8
Angel Esparza <angel.esparza@unity3d.com>
2024-01-20 18:38:50 -0800

    New background

--- Assets/Scripts/BackgroundPanning.cs
+++ Assets/Scripts/BackgroundPanning.cs
@@ -4,51 +4,58 @@ using UnityEngine;
 
 public class BackgroundPanning : MonoBehaviour
 {
-    [SerializeField] GameObject backgroundVertical;
-    [SerializeField] GameObject backgroundHorizontal;
-    [SerializeField] float panningSpeed;
-    float HorizontalPanningSpeed;
-    float scrollVerticalLimit = 79.4f;
-    bool moveRight;
+    [SerializeField] float rotationSpeed;
 
-    private void Start()
+    private void Update()
     {
-        HorizontalPanningSpeed = panningSpeed * 1.5f; 
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 
-    void Update()
-    {
-        
-        backgroundVertical.transform.position += new Vector3(0f, panningSpeed*Time.deltaTime, 0f);
-        
+    //[SerializeField] GameObject backgroundVertical;
+    //[SerializeField] GameObject backgroundHorizontal;
+    //[SerializeField] float panningSpeed;
+    //float HorizontalPanningSpeed;
+    //float scrollVerticalLimit = 79.4f;
+    //bool moveRight;
 
-        if (backgroundVertical.transform.position.y >= scrollVerticalLimit)
-        {
-            backgroundVertical.transform.position = new Vector3(0f, 0f, 0f);
-        }
+    //private void Start()
+    //{
+    //    HorizontalPanningSpeed = panningSpeed * 1.5f; 
+    //}
 
-        //Horizontal
+    //void Update()
+    //{
 
-        if (!moveRight && backgroundHorizontal.transform.position.x > -50)
-        {
-            backgroundHorizontal.transform.position -= new Vector3(HorizontalPanningSpeed * Time.deltaTime, 0f, 0f);
-        }
-        else
-        {
-            moveRight = true;
-
-        }
-
-        if (moveRight && backgroundHorizontal.transform.position.x < 0)
-        {
-            backgroundHorizontal.transform.position += new Vector3(HorizontalPanningSpeed * Time.deltaTime, 0f, 0f);
-        }
-        else
-        {
-            moveRight = false;
-        }
+    //    backgroundVertical.transform.position += new Vector3(0f, panningSpeed*Time.deltaTime, 0f);
 
 
+    //    if (backgroundVertical.transform.position.y >= scrollVerticalLimit)
+    //    {
+    //        backgroundVertical.transform.position = new Vector3(0f, 0f, 0f);
+    //    }
 
-    }
+    //    //Horizontal
+
+    //    if (!moveRight && backgroundHorizontal.transform.position.x > -50)
+    //    {
+    //        backgroundHorizontal.transform.position -= new Vector3(HorizontalPanningSpeed * Time.deltaTime, 0f, 0f);
+    //    }
+    //    else
+    //    {
+    //        moveRight = true;
+
+    //    }
+
+    //    if (moveRight && backgroundHorizontal.transform.position.x < 0)
+    //    {
+    //        backgroundHorizontal.transform.position += new Vector3(HorizontalPanningSpeed * Time.deltaTime, 0f, 0f);
+    //    }
+    //    else
+    //    {
+    //        moveRight = false;
+    //    }
+
+
+
+    //}
 }
